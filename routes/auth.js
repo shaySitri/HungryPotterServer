82ea7e950096a5167693078403f89d894e917fb4
ShaySitri <sitri@post.bgu.ac.il>
2023-05-27 17:50:00 +0300

    SOME UPDATES

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,6 @@ router.post("/Register", async (req, res, next) => {
       country: req.body.country,
       password: req.body.password,
       email: req.body.email,
-      profilePic: req.body.profilePic
     }
     let users = [];
     users = await DButils.execQuery("SELECT username from users");
@@ -30,7 +29,8 @@ router.post("/Register", async (req, res, next) => {
       parseInt(process.env.bcrypt_saltRounds)
     );
     await DButils.execQuery(
-      `INSERT INTO users VALUES ('${user_details.username}', '${user_details.firstname}', '${user_details.lastname}',
+      `INSERT INTO users (username,firstname,lastname,country,password,email) 
+      VALUES ('${user_details.username}', '${user_details.firstname}', '${user_details.lastname}',
       '${user_details.country}', '${hash_password}', '${user_details.email}')`
     );
     res.status(201).send({ message: "user created", success: true });
--- sql scripts/Create.sql
+++ sql scripts/Create.sql
@@ -1,3 +1,11 @@
 CREATE DATABASE hungry_potter;
 USE hungry_potter;
+CREATE TABLE users (
+    username VARCHAR(255),
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
+    country VARCHAR(255),
+    password VARCHAR(255),
+    email VARCHAR(255)
+);
 
