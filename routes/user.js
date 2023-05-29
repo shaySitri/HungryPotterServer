281b22343fd3f294543e7bee2fad029054a306cb
ShaySitri <sitri@post.bgu.ac.il>
2023-05-30 00:37:03 +0300

    last changes

--- routes/user.js
+++ routes/user.js
@@ -3,22 +3,66 @@ var router = express.Router();
 const DButils = require("./utils/DButils");
 const user_utils = require("./utils/user_utils");
 const recipe_utils = require("./utils/recipes_utils");
+let userRecipeCounter = 0;
+
 
 /**
  * Authenticate all incoming requests by middleware
  */
-router.use(async function (req, res, next) {
-  if (req.session && req.session.user_id) {
-    DButils.execQuery("SELECT user_id FROM users").then((users) => {
-      if (users.find((x) => x.user_id === req.session.user_id)) {
-        req.user_id = req.session.user_id;
-        next();
-      }
-    }).catch(err => next(err));
-  } else {
-    res.sendStatus(401);
+// router.use(async function (req, res, next) {
+//   if (req.session && req.session.user_id) {
+//     DButils.execQuery("SELECT user_id FROM users").then((users) => {
+//       if (users.find((x) => x.user_id === req.session.user_id)) {
+//         req.user_id = req.session.user_id;
+//         next();
+//       }
+//     }).catch(err => next(err));
+//   } else {
+//     res.sendStatus(401);
+//   }
+// });
+
+
+/**
+ * This path gets body with recipeId and save this recipe in the favorites list of the logged-in user
+ */
+router.post('/addRecipe', async (req,res,next) => {
+
+  
+ 
+  try{
+
+    
+    let recipeDeatils =
+    {
+      //username: req.session.user_id,
+      username: "try1",
+      recipeId: userRecipeCounter + 1,
+      title: req.body.title,
+      readyInMinutes: req.body.readyInMinutes,
+      image:  req.body.image,
+      popularity: 0,
+      vegan: req.body.vegan,
+      vegetarian: req.body.vegetarian,
+      glutenFree: req.body.glutenFree,
+      ingredients: req.body.ingredients,
+      instructions: req.body.instructions,
+      servings: req.body.servings
+    }
+
+    console.log("xxx")
+    console.log("xxx" ,req.body.title)
+
+
+    await user_utils.addNewRecipe(recipeDeatils);
+    res.status(200).send("The Recipe Addedd Successfully");
+
+    userRecipeCounter++;
+
+    } catch(error){
+    next(error);
   }
-});
+})
 
 
 /**
--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -10,6 +10,44 @@ async function getFavoriteRecipes(user_id){
 }
 
 
+function isURL(url)
+{
+    return /\.(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?$/.test(url);
+}
+function isImage(url) {
+    return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
+  }
+function containsOnlyNumbers(str) {
+    return /^\d+$/.test(str);
+}
+async function addNewRecipe(recipeDeatils){
+
+    console.log("typof s:",typeof recipeDeatils.servings,"typeof rim:",recipeDeatils.readyInMinutes)
+    if ((containsOnlyNumbers(recipeDeatils.readyInMinutes) != 'true') || (containsOnlyNumbers(recipeDeatils.servings) != 'true'))
+    {
+        throw { status: 409, message: "Value unvaliable." };
+    }
+    if ((isURL(recipeDeatils.image) == false) || (isImage(recipeDeatils.image)))
+    {
+        throw { status: 409, message: "Image unvaliable." };
+    }
+    if ((typeof recipeDeatils.vegan != "boolean") || (typeof recipeDeatils.vegetarian != "boolean") || (typeof recipeDeatils.glutenFree != "boolean"))
+    {
+        throw { status: 409, message: "Value boolean unvaliable." };
+    }
+
+    // how to check the ingrediants???
+
+    const recipes_id = await DButils.execQuery(
+        `INSERT INTO userRecipes (username,recipeId,title,readyInMinutes,image,popularity,vegan,vegetarian,glutenFree,ingredients,instructions,servings) 
+        VALUES ('${recipeDeatils.username}', '${recipeDeatils.recipeId}', '${recipeDeatils.title}',
+        '${recipeDeatils.readyInMinutes}', '${recipeDeatils.image}', '${recipeDeatils.popularity}',
+        '${recipeDeatils.vegan}', '${recipeDeatils.vegetarian}', '${recipeDeatils.glutenFree}',
+        '${recipeDeatils.ingredients}','${recipeDeatils.instructions}','${recipeDeatils.servings}');`
+      );
+    return recipes_id;
+}
 
 exports.markAsFavorite = markAsFavorite;
 exports.getFavoriteRecipes = getFavoriteRecipes;
+exports.addNewRecipe = addNewRecipe;
